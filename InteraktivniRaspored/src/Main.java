6b7d22ba7123047c519b78749e2b0b99ded3118a
dinooo <dino00_06@hotmail.com>
2016-02-13 04:48:34 +0100

    Izmjena samo u pozivu

--- InteraktivniRaspored/src/Main.java
+++ InteraktivniRaspored/src/Main.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 
 import dataBase.DBExecuteNastavnik;
 import gui.LoginScreen;
+import gui.ProdekanGUI;
 
 
 
@@ -9,9 +10,12 @@ import gui.LoginScreen;
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
-		DBExecuteNastavnik.connect();
+		//DBExecuteNastavnik.getNastavnici();
 		
-		LoginScreen.startLoginScreen();
+		//LoginScreen.startLoginScreen();
+		
+		ProdekanGUI.startProdekanGUI();
+		
 	}
 
 }
